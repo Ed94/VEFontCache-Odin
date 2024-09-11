922372b582c07a849c0091a9497e27dcfc95e0e8
Ed_ <edwardgz@gmail.com>
2024-09-11 15:11:24 -0400

    Fixes for uv coords on shaders when compiling for opengl vs directx

--- backend/sokol/source_shared.shdc.glsl
+++ backend/sokol/source_shared.shdc.glsl
@@ -5,6 +5,10 @@ out vec2 uv;
 
 void main()
 {
-	uv          = vec2( v_texture.x, 1 - v_texture.y );
+#if SOKOL_GLSL
+	uv          = vec2( v_texture.x, v_texture.y );
+#else
+	uv          = vec2( v_texture.x, 1.0 - v_texture.y );
+#endif
 	gl_Position = vec4( v_position, 0.0, 1.0 );
 }
