6bc7514ef05c964587c852a08ba0cbd1722fb79b
Ed_ <edwardgz@gmail.com>
2025-01-13 12:04:36 -0500

    force script to be LF (not sure why it wasnt...)

--- scripts/build_sokol_demo.sh
+++ scripts/build_sokol_demo.sh
@@ -84,7 +84,7 @@ popd > /dev/null
 path_stb_truetype="$path_thirdparty/stb/src"
 
 pushd "$path_stb_truetype" > /dev/null
-    eval make
+    eval make -C "$path_stb_truetype"
 popd > /dev/null
 
 source "$(dirname "$0")/helpers/odin_compiler_defs.sh"
