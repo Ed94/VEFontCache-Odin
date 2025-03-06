17a061a7906baa4b89fdc09ab48a5eddd7f8257e
Ed_ <edwardgz@gmail.com>
2025-03-06 08:11:42 -0500

    remove stb_image.c from stb/src/build.bat (oops)
    stb_truetype will be removed sometime in the future as there is a good ttf parser available in a fork.

--- thirdparty/stb/src/build.bat
+++ thirdparty/stb/src/build.bat
@@ -2,7 +2,7 @@
 
 if not exist "..\lib" mkdir ..\lib
 
-cl -nologo -MT -TC -O2 -c stb_image.c stb_truetype.c
+cl -nologo -MT -TC -O2 -c stb_truetype.c
 lib -nologo stb_truetype.obj -out:..\lib\stb_truetype.lib
 
 del *.obj
